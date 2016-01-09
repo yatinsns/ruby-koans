247942c24d5a18a1f929447da2bdf6d6180456cc
Yatin Sarbalia <yatinsns@gmail.com>
2016-01-09 19:02:37 +0530

    Finish koan 155

--- triangle.rb
+++ triangle.rb
@@ -17,6 +17,7 @@ def triangle(a, b, c)
   # WRITE THIS CODE
   return :equilateral if a == b and b == c
   return :isosceles if a == b or b == c or c == a
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
