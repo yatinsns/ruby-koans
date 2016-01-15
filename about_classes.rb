19c501817ffe9c9f95739bbfb0e9adf2f955432c
Yatin Sarbalia <yatinsns@gmail.com>
2016-01-16 01:29:59 +0530

    Finish koan 204

--- about_classes.rb
+++ about_classes.rb
@@ -159,7 +159,7 @@ class AboutClasses < Neo::Koan
     fido = Dog7.new("Fido")
 
     fidos_self = fido.get_self
-    assert_equal __, fidos_self
+    assert_equal fido, fidos_self
   end
 
   def test_to_s_provides_a_string_version_of_the_object
